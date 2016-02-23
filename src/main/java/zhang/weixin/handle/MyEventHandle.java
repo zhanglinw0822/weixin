0adb337146035aad2a019929a9e1656705d74207
unknown <zhanglinw0822@163.com>
2016-02-23 15:54:45 +0800

    微信框架初始化

---
+++ src/main/java/zhang/weixin/handle/MyEventHandle.java
@@ -0,0 +1,19 @@
+package zhang.weixin.handle;
+
+import com.github.sd4324530.fastweixin.handle.EventHandle;
+import com.github.sd4324530.fastweixin.message.BaseMsg;
+import com.github.sd4324530.fastweixin.message.req.BaseEvent;
+
+public class MyEventHandle implements EventHandle{
+
+	public BaseMsg handle(BaseEvent event) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean beforeHandle(BaseEvent event) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
---
+++ src/main/java/zhang/weixin/handle/MyMessageHandle.java
@@ -0,0 +1,19 @@
+package zhang.weixin.handle;
+
+import com.github.sd4324530.fastweixin.handle.MessageHandle;
+import com.github.sd4324530.fastweixin.message.BaseMsg;
+import com.github.sd4324530.fastweixin.message.req.BaseReqMsg;
+
+public class MyMessageHandle implements MessageHandle{
+
+	public BaseMsg handle(BaseReqMsg message) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean beforeHandle(BaseReqMsg message) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
